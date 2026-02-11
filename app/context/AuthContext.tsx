6780394863bcdf7963abe1166e64e35845549e3e
elitam <elinski@gmail.com>
2026-02-11 14:01:10 +0100

    users

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -5,7 +5,11 @@ import { motion, AnimatePresence } from 'framer-motion'
 const USERS_MAP: any = {
   "5368": { initials: "ET", role: "admin" },
   "2410": { initials: "ZZ", role: "user" },
-  "2512": { initials: "CR", role: "admin" }
+  "2512": { initials: "CR", role: "admin" },
+  "3006": { initials: "ML", role: "user" },
+  "1515": { initials: "TM", role: "user" }
+
+
 }
 
 const AuthContext = createContext<any>({ user: null, logout: () => {} })
